281337d577eb54a9e6dbb9208f834fb5691e75f6
KRSL367 <krishalpudasaini12345@gmail.com>
2024-06-17 17:14:33 +0545

    Updatad the urls

--- laboratory/urls.py
+++ laboratory/urls.py
@@ -1,18 +1,44 @@
-# laboratory/urls.py
-from django.urls import path, include
-from rest_framework_nested import routers
-from .views import (
-    PatientViewSet,
-    MedicalDataViewSet,
-    DiagnosisReportViewSet,
-    HospitalViewSet,
-)
+# # laboratory/urls.py
+# from django.urls import path, include
+# from rest_framework_nested import routers
+# from .views import (
+#     PatientViewSet,
+#     MedicalDataViewSet,
+#     DiagnosisReportViewSet,
+#     HospitalViewSet,
+# )
 
-# router = DefaultRouter()
-# router.register("patients", PatientViewSet, basename="patient")
-# router.register("medical-images", MedicalImageViewSet, basename="medicalimage")
-# router.register("diagnosis-reports", DiagnosisReportViewSet)
+# router = routers.DefaultRouter()
 # router.register("hospitals", HospitalViewSet, basename="hospital")
+
+# hospital_router = routers.NestedDefaultRouter(router, "hospitals", lookup="hospital")
+# hospital_router.register("patients", PatientViewSet, basename="hospital-patients")
+
+# patient_router = routers.NestedDefaultRouter(
+#     hospital_router, "patients", lookup="patient"
+# )
+# patient_router.register(
+#     "medical-datas", MedicalDataViewSet, basename="patient-medical-data"
+# )
+
+# medical_data_router = routers.NestedDefaultRouter(
+#     patient_router, "medical-datas", lookup="medical_data"
+# )
+# medical_data_router.register(
+#     "diagnosis-reports",
+#     DiagnosisReportViewSet,
+#     basename="medical-data-diagnosis-reports",
+# )
+
+# urlpatterns = router.urls + hospital_router.urls + patient_router.urls + medical_data_router.urls 
+
+
+from rest_framework_nested import routers
+from django.urls import path, include
+from .views import (
+    HospitalViewSet, PatientViewSet, MedicalDataViewSet, 
+    MedicalDataImagesViewSet, DiagnosisReportViewSet, DiagnosisReportImageViewSet
+)
 
 router = routers.DefaultRouter()
 router.register("hospitals", HospitalViewSet, basename="hospital")
@@ -31,9 +57,19 @@ medical_data_router = routers.NestedDefaultRouter(
     patient_router, "medical-datas", lookup="medical_data"
 )
 medical_data_router.register(
+    "medical-images", MedicalDataImagesViewSet, basename="medical-data-images"
+)
+medical_data_router.register(
     "diagnosis-reports",
     DiagnosisReportViewSet,
     basename="medical-data-diagnosis-reports",
 )
 
-urlpatterns = router.urls + hospital_router.urls + patient_router.urls + medical_data_router.urls 
+diagnosis_report_router = routers.NestedDefaultRouter(
+    medical_data_router, "diagnosis-reports", lookup="diagnosis_report"
+)
+diagnosis_report_router.register(
+    "images", DiagnosisReportImageViewSet, basename="diagnosis-report-images"
+)
+
+urlpatterns = router.urls + hospital_router.urls + patient_router.urls + medical_data_router.urls + diagnosis_report_router.urls
