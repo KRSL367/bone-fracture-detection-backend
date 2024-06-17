1a382cd022bc861df48101b5373bbe2fa7bcf318
KRSL367 <krishalpudasaini12345@gmail.com>
2024-06-17 14:59:34 +0545

    Updated Patient, Mediaclimage, and diagnosisreport viewsets

--- laboratory/views.py
+++ laboratory/views.py
@@ -8,18 +8,26 @@ from .serializers import (
     HospitalSerializer,
 )
 
-class PatientViewSet(viewsets.ModelViewSet):
-    queryset = Patient.objects.all()
-    serializer_class = PatientSerializer
-
-class MedicalImageViewSet(viewsets.ModelViewSet):
-    queryset = MedicalImage.objects.all()
-    serializer_class = MedicalImageSerializer
-
-class DiagnosisReportViewSet(viewsets.ModelViewSet):
-    queryset = DiagnosisReport.objects.all()
-    serializer_class = DiagnosisReportSerializer
 
 class HospitalViewSet(viewsets.ModelViewSet):
     queryset = Hospital.objects.all()
     serializer_class = HospitalSerializer
+    
+class PatientViewSet(viewsets.ModelViewSet):
+    serializer_class = PatientSerializer
+
+    def get_queryset(self):
+        return Patient.objects.filter(hospital_id=self.kwargs['hospital_pk'])
+
+class MedicalImageViewSet(viewsets.ModelViewSet):
+    serializer_class = MedicalImageSerializer
+
+    def get_queryset(self):
+        return MedicalImage.objects.filter(patient_id=self.kwargs['patient_pk'])
+
+
+class DiagnosisReportViewSet(viewsets.ModelViewSet):
+    serializer_class = DiagnosisReportSerializer
+
+    def get_queryset(self):
+        return DiagnosisReport.objects.filter(medical_image_id=self.kwargs['medical_image_pk'])
