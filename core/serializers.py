8f2fe1df77fd2c84551853fdcd2d4928199530b4
KRSL367 <krishalpudasaini12345@gmail.com>
2024-07-29 14:23:30 +0545

    Overriding token create endpoint

--- core/serializers.py
+++ core/serializers.py
@@ -3,10 +3,12 @@ from djoser.serializers import (
     UserSerializer as BaseUserSerializer,
     UserCreateSerializer as BaseUserCreateSerializer,
 )
+from django.contrib.auth import authenticate
 from rest_framework import serializers
-from .models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from laboratory.models import Hospital
 from laboratory.serializers import HospitalSerializer
+from .models import User
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     hospital_name = serializers.CharField(write_only=True, required=True)
@@ -46,6 +48,45 @@ class UserSerializer(BaseUserSerializer):
         fields = ["id", "username", "email", "first_name", "last_name", "hospital"]
 
 
+class CustomTokenCreateSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(
+        style={"input_type": "password"}, trim_whitespace=False
+    )
 
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        if username and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                username=username,
+                password=password,
+            )
+
+            if not user:
+                msg = "Unable to log in with provided credentials."
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = 'Must include "username" and "password".'
+            raise serializers.ValidationError(msg, code="authorization")
+
+        refresh = RefreshToken.for_user(user)
+
+        data = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "is_hospital_admin": user.is_hospital_admin,
+                "is_superuser": user.is_superuser,
+                "full_name": user.first_name + " " + user.last_name,
+                "hospital_id": user.hospital_id,
+            },
+        }
+
+        return data
 
 
