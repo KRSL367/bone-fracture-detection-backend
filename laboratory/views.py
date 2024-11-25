009b5be58188f71145e6ac2523a99689a5dc533f
KRSL367 <krishalpudasaini12345@gmail.com>
2024-11-25 14:41:59 +0545

    modified medical data image end point to handle batch upload and batch delete

--- laboratory/views.py
+++ laboratory/views.py
@@ -2,6 +2,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 from laboratory.pagination import DefaultPagination
 from .models import Patient, MedicalData, DiagnosisReport, Hospital, MedicalDataImages, DiagnosisReportImages
 from .serializers import (
@@ -63,6 +66,35 @@ class MedicalDataImagesViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return MedicalDataImages.objects.filter(medical_data_id=self.kwargs['medical_data_pk'])
+    
+    def create(self, request, *args, **kwargs):
+        # Expecting files in request.FILES
+        files = request.FILES.getlist("image")
+        medical_data_id = self.kwargs["medical_data_pk"]
+        data = [{"image": file, "medical_data": medical_data_id} for file in files]
+
+        serializer = self.get_serializer(data=data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    @action(detail=False, methods=["post"], url_path="batch-delete")
+    def batch_delete(self, request, *args, **kwargs):
+        # Collect all image_ids from the request data
+        image_ids = request.data.getlist("image_ids")
+        if not image_ids:
+            return Response(
+                {"error": "No image IDs provided"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        images = MedicalDataImages.objects.filter(id__in=image_ids)
+        count, _ = images.delete()
+        return Response({"deleted": count}, status=status.HTTP_204_NO_CONTENT)
+
 
 class DiagnosisReportViewSet(viewsets.ModelViewSet):
     serializer_class = DiagnosisReportSerializer
