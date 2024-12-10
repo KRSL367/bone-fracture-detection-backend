94ff8ffab7be652a515bb4438eb93a2468dc3b0f
KRSL367 <krishalpudasaini12345@gmail.com>
2024-12-10 22:02:19 +0545

    Tests

--- laboratory/views.py
+++ laboratory/views.py
@@ -1,4 +1,8 @@
+from django.forms import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
+import requests
+from django.db import transaction
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -94,6 +98,81 @@ class MedicalDataImagesViewSet(viewsets.ModelViewSet):
         images = MedicalDataImages.objects.filter(id__in=image_ids)
         count, _ = images.delete()
         return Response({"deleted": count}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+    @action(detail=False, methods=['post'], url_path='images-for-x-ray-check')
+    def images_for_xray_check(self, request, *args, **kwargs):
+        medical_id = request.data.get("medical_id")
+        if not medical_id:
+            return Response({"error": "Medical ID not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            # Fetch images associated with the blood test ID
+            images = MedicalDataImages.objects.filter(medical_data_id=medical_id)
+            if not images.exists():
+                return Response({"error": "No images found for the provided Medical ID"}, status=status.HTTP_404_NOT_FOUND)
+        except MedicalDataImages.DoesNotExist:
+            return Response({"error": "X-Ray test data not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Construct absolute URLs for the images
+        image_urls = [request.build_absolute_uri(image.image.url) for image in images]
+
+        # Prepare payload to send to FastAPI
+        data = {
+            "urls": image_urls
+        }
+        
+        print(data)
+
+        # URL of your FastAPI endpoints
+        x_ray_api_url = 'http://127.0.0.1:7001/process_images/'
+
+        try:
+            # Send POST request to blood cell count FastAPI
+            response_x_ray = requests.post(x_ray_api_url, json=data,  # Pass data as JSON
+                headers={"Content-Type": "application/json"})
+            response_x_ray.raise_for_status()
+            response_x_ray_response = response_x_ray.json()
+
+            # Extract results from the response
+            x_ray = response_x_ray_response["results"]
+            x_ray_detected = x_ray["detected"]  # List of detected conditions/features
+            processed_images = x_ray["processed_image_urls"]  # List of processed image URLs
+
+            # Ensure detected descriptions match the number of images
+            if len(x_ray_detected) != len(processed_images):
+                raise ValueError("Mismatch between detected features and processed image URLs.")
+
+                    
+            # Use a transaction to ensure atomicity
+            with transaction.atomic():
+                # Create a new DiagnosisReport instance
+                diagnosis_report = DiagnosisReport.objects.create(
+                    report="Diagnosed Image Results",
+                    medical_data_id=medical_id
+                )
+
+                 # Save the processed image links and their corresponding descriptions
+                for image_url, description in zip(processed_images, x_ray_detected):
+                    DiagnosisReportImages.objects.create(
+                        diagnosis_report=diagnosis_report,
+                        image=image_url,
+                        description=description  # Assuming a description field exists in the model
+                    )
+        
+                return Response({"message": "Data stored successfully"}, status=status.HTTP_201_CREATED)
+
+        except requests.exceptions.RequestException as e:
+            error_message = f"Error sending images to FastAPI: {str(e)}"
+            return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            error_message = f"Unexpected error: {str(e)}"
+            return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
 
 
 class DiagnosisReportViewSet(viewsets.ModelViewSet):
---
+++ tests/test_hospitals.py
@@ -0,0 +1,118 @@
+from rest_framework import status
+import pytest
+from model_bakery import baker
+from laboratory.models import Hospital
+
+@pytest.fixture
+def create_hospital(api_client):
+    def do_create_hospital(hospital):
+        return api_client.post("/laboratory/hospitals/", hospital)
+
+    return do_create_hospital
+
+
+@pytest.mark.django_db
+class TestCreateHospital:
+
+    # def test_if_user_is_anonymous_returns_401(self, create_hospital):
+
+    #     # Arrange
+
+    #     # Act
+    #     response = create_hospital(
+    #         {
+    #             "name": "a",
+    #             "phone": "a",
+    #         }
+    #     )
+
+    #     # Assert
+    #     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    # def test_if_user_is_not_admin_returns_403(self, create_hospital, authenticate):
+
+    #     # Arrange
+    #     authenticate()
+
+    #     # Act
+    #     response = create_hospital(
+    #         {
+    #             "name": "a",
+    #             "phone": "a",
+    #         }
+    #     )
+
+    #     # Assert
+    #     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, create_hospital, authenticate):
+        # Arrange
+        authenticate(is_superuser=True)
+
+        # Act
+        response = create_hospital(
+            {
+                "name": "",
+                "phone": "a",
+            }
+        )
+
+        # Assert
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_if_data_is_valid_returns_200(
+        self, api_client, create_hospital, authenticate
+    ):
+
+        # Arrange
+        authenticate(is_superuser=True)
+
+        # Act
+        response = create_hospital(
+            {"name": "a", "phone": "a"}
+        )
+
+        # Assert
+        assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+class TestRetrieveHospital:
+
+    # def test_if_hospital_exists_and_user_is_not_admin_returns_403(
+    #     self, api_client, authenticate
+    # ):
+
+    #     authenticate()
+
+    #     hospital = baker.make(Hospital)
+
+    #     response = api_client.get(f"/hospitals/{hospital.id}/")
+
+    #     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    # def test_if_hospital_exists_and_user_is_anonymous_returns_401(self, api_client):
+
+    #     hospital = baker.make(Hospital)
+
+    #     response = api_client.get(f"/hospitals/{hospital.id}/")
+
+    #     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    # def test_if_hospital_exists_returns_200(self, api_client, authenticate):
+
+    #     authenticate(is_superuser=True)
+
+    #     hospital = baker.make(Hospital)
+
+    #     response = api_client.get(f"/hospitals/{hospital.id}/")
+
+    #     assert response.status_code == status.HTTP_200_OK
+
+    def test_if_hospital_does_not_exists_returns_404(self, api_client, authenticate):
+
+        authenticate(is_superuser=True)
+
+        response = api_client.get("/hospitals/1/")
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
